4803eb6854d0930bdaaa77d7476cb24d3c74b0f0
Carla-life <Carlalife84@gmail.com>
2024-06-12 23:26:44 +0100

    Update watbridge.py

--- watbridge.py
+++ watbridge.py
@@ -5,7 +5,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2016 Rafael Medina García <rafamedgar@gmail.com>
+# Copyright (c) 2024 carla life <Carlalife84@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
