d8ac6caba53f90f49565e9e027e729a7629bc7e9
Carla-life <Carlalife84@gmail.com>
2024-06-12 23:00:44 +0100

    Update watbridge.py
    Telegram-WhatsApp

--- watbridge.py
+++ watbridge.py
@@ -1,40 +1,40 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 #
-# wat-bridge
+#Wat-bridge
 # https://github.com/rmed/wat-bridge
 #
-# The MIT License (MIT)
+# The MIT license (MIT)
 #
-# Copyright (c) 2016 Rafael Medina García <rafamedgar@gmail.com>
+# Copyright (c) 2024 Carla life <Carlalife84@gmail.com>
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
+# Permission is granted free of charge to anyone obtaining a copy
+# of this software and associated documentation files (the “Software”), to process
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
+# to use, copy, modify, merge, publish, distribute, sublicense and/or sell
+# copies of the Software, and to allow persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice must be included in all
+# copies or substantial parts of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT
+# AUTHORS OR COPYRIGHT HOLDERS ARE LIABLE FOR ANY CLAIMS, DAMAGES OR OTHERWISE
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER MATTERS IN THE
 # SOFTWARE.
 
-"""Main launcher."""
+"""Main Launcher."""
 
 from __future__ import print_function
 
 import signal
-import threading
+import a thread
 
 from wat_bridge.static import SIGNAL_TG, SIGNAL_WA, init_bridge
 
-# Parse config file
+# Parse configuration file
 init_bridge()
 
 from wat_bridge.listeners import tg_listener, wa_listener
@@ -42,14 +42,14 @@ from wat_bridge.signals import sigint_handler, to_tg_handler, to_wa_handler
 
 
 if __name__ == '__main__':
-    # Setup signaling
+    # Signaling configuration
     signal.signal(signal.SIGINT, sigint_handler)
     print('Press Ctrl+C to exit')
 
     SIGNAL_TG.connect(to_tg_handler)
-    SIGNAL_WA.connect(to_wa_handler)
+SIGNAL_WA.connect(to_wa_handler)
 
-    # Launch threads and wait for them
+# Start discussions and wait for them
     tg_thread = threading.Thread(target=tg_listener)
     wa_thread = threading.Thread(target=wa_listener)
 
